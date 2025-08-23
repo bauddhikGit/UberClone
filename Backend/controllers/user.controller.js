1f0e6445c2b52501c4e104d4b1ff53a667d2673c
Bauddhik <bauddhikkandpal@gmail.com>
2025-08-23 11:53:10 +0530

    update captain model vehicle type enum

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -38,6 +38,7 @@ module.exports.loginUser = async (req, res, next) => {
   const { email, password } = req.body;
 
   const user = await userModel.findOne({ email }).select("+password");
+
   if (!user) {
     return res.status(401).json({ message: "Invalid email or password" });
   }
