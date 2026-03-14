9a8237c78ae30fc760387f1633bdfbb72da1aeb7
Bauddhik <bauddhikkandpal@gmail.com>
2026-03-14 15:48:29 +0530

    backend changes

--- Backend/app.js
+++ Backend/app.js
@@ -1,3 +1,5 @@
+// 8:18
+
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
@@ -7,6 +9,8 @@ const cookieParser = require("cookie-parser");
 const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 const captainRoutes = require("./routes/captain.routes");
+const mapsRoutes = require("./routes/maps.routes");
+const rideRoutes = require("./routes/ride.routes");
 
 connectToDb();
 
@@ -20,5 +24,7 @@ app.get("/", (req, res) => {
 });
 app.use("/users", userRoutes);
 app.use("/captains", captainRoutes);
+app.use("/maps", mapsRoutes);
+app.use("/rides", rideRoutes);
 
 module.exports = app;
